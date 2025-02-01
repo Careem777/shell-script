30cbfc42f6265c44dd887b7d3e99885aee51da46
Careem <careemdev7@gmail.com>
2025-02-02 00:42:16 +0530

    Variables run time

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1 : Hi $PERSION2"
+echo "$PERSION2: hello $PERSION1"
+echo "$PERSION1 : How are you doing?"
+echo "$PERSION2: I am good. How are you doing?"
