bf08cb57f8dc32ad4080164e1b2a22cc94e8500e
Careem <careemdev7@gmail.com>
2025-02-02 00:58:21 +0530

    Variables run time

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo " please enter username:"
-read USERNAME # USERNAME is variable here
+echo " please enter username:" # text entered here will be added as value to variable
+
+read USERNAME # USERNAME is variable name here
+echo  "username entered : $USERNAME"
 
 
